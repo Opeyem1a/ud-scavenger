57d7f36f7fb04d112ecbf0fcec4df6804b930f82
opeyem1a <opeyadeyemi@gmail.com>
2022-03-09 18:38:34 -0800

    + rename NavBar to Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type {NextPage} from 'next'
-import NavBar from "../components/Header";
+import Button from "../components/Button";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
     return (
         <>
-            <NavBar/>
+            <Header/>
+            <Button content={'Something'} style={'primary'}/>
         </>
     )
 }
